d0e9cfb0cca631babb49098ee017ca275becf96c
cookie <284029652@qq.com>
2016-04-20 18:22:10 +0800

    change

---
+++ js/controllers/homeCtrl.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Administrator on 2016/4/18.
+ */
+
+function homeCtrl($scope) {
+    var slider = mui("#slider");
+    slider.slider({
+        interval: 0
+    });
+
+}
--- js/main.js
+++ js/main.js
@@ -1,43 +1,47 @@
 /**
  * Created by Administrator on 2016/4/18.
  */
-/**
- * Created by Administrator on 2016/4/18.
- */
 
-angular.module('ionicApp', ['ionic'])
-
-    .config(function($stateProvider, $urlRouterProvider) {
-
-        $stateProvider
-            .state('tabs', {
-                url: "/tab",
-                abstract: true,
-                templateUrl: "tabs/tabs.html"
-            })
-            .state('tabs.home', {
-                url: "/home",
-                views: {
-                    'home-tab': {
-                        templateUrl: "tabs/home-page.html",
-                        controller: 'HomeTabCtrl'
-                    }
-                }
-            })
-            .state('tabs.about', {
-                url: "/about",
-                views: {
-                    'about-tab': {
-                        templateUrl: "tabs/latest-supply.html"
-                    }
-                }
-            });
-
-
-        $urlRouterProvider.otherwise("/tab/home");
-
-    })
-
-    .controller('HomeTabCtrl', function($scope) {
-        console.log('HomeTabCtrl');
+var myApp = angular.module('myApp', ['ui.router']);
+// For Component users, it should look like this:
+// var myApp = angular.module('myApp', [require('angular-ui-router')]);
+myApp.config(function($stateProvider, $urlRouterProvider) {
+    //
+    // For any unmatched url, redirect to /state1
+    $urlRouterProvider.otherwise("/homePage");
+    //
+    // Now set up the states
+    $stateProvider
+        .state('homePage', {
+            url: "/homePage",
+            templateUrl: "tabs/home-page.html",
+            controller: "homeCtrl"
+        })
+        .state('state1.list', {
+            url: "/list",
+            templateUrl: "partials/state1.list.html",
+            controller: function($scope) {
+                $scope.items = ["A", "List", "Of", "Items"];
+            }
+        })
+        .state('latestSupply', {
+            url: "/latestSupply",
+            templateUrl: "tabs/latest-supply.html"
+        })
+        .state('newestBuy', {
+            url: "/newestBuy",
+            templateUrl: "tabs/newest-buy.html"
+        })
+        .state('shopCar', {
+            url: "/shopCar",
+            templateUrl: "tabs/shop-car.html"
+        })
+        .state('mine', {
+            url: "/mine",
+            templateUrl: "tabs/mine.html"
     });
+});
+
+(function() {
+
+}());
